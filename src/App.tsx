cf26852fe75d7aaf26489db1216b9a027b46c44d
Tatsuya Teshima <terima666tatsuya@yahoo.co.jp>
2020-09-13 22:48:45 +0900

    add ApolloProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Main from "./components/Main";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-function App() {
+const client = new ApolloClient({
+  uri: "",
+  cache: new InMemoryCache(),
+});
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Main />
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
